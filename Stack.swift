2eb47d78b9105eff0939ce12a96918c091fe09b7
Ravindra <ravindra.psc@gmail.com>
2017-11-14 16:39:58 +0530

    Add additional methods and make Stack StringConvertible
    These methods provided extra support to work with Stack

--- Stack.swift
+++ Stack.swift
@@ -12,26 +12,64 @@ enum StackError: Error {
     case emptyStack
 }
 
-struct Stack<Element> {
+struct Stack<Element> : CustomStringConvertible {
+
     private var array = [Element]()
-    
-    mutating func push(element: Element) {
-        array.append(element)
+
+    public var count: Int { return self.array.count }
+    public var isEmpty: Bool { return self.array.isEmpty }
+    public var description: String {
+        return self.array.reversed().reduce("Stack[Top:]"){"\($0) --> \($1)"}
     }
     
-    mutating func pop() throws -> Element? {
-        if array.isEmpty {throw StackError.emptyStack}
+    public mutating func push(element: Element) { self.array.append(element) }
+    
+    public mutating func pop() throws -> Element? {
+        if array.isEmpty { throw StackError.emptyStack }
         let element = array.removeLast()
         return element
     }
     
-    func top() throws -> Element? {
-        if array.isEmpty {throw StackError.emptyStack}
-        return array.last
+    public func top() throws -> Element? {
+        if self.array.isEmpty { throw StackError.emptyStack }
+        return self.array.last
     }
+}
 
-    func description() {
-        print(self.array, separator: "-->", terminator: "\n")
+extension Stack {
+    
+    enum MergeStrategy {
+        case oneByOne
+        case allAtOnce
+    }
+    
+    func reversed() -> Stack<Element> {
+        let nArray = self.array.reversed()
+        var nStack = Stack<Element>()
+        
+        for i in 0 ..< nArray.count {
+            let idx = nArray.index(nArray.startIndex, offsetBy: i)
+            nStack.push(element: nArray[idx])
+        }
+        
+        return nStack
+    }
+    
+    func merge(_ otherStack: Stack<Element>, strategy: Stack.MergeStrategy = .oneByOne) -> Stack<Element> {
+        var nStack = self
+        var other = otherStack
+        switch strategy {
+        case .oneByOne: other = otherStack
+        case .allAtOnce: other = otherStack.reversed()
+        }
+        
+        while !other.isEmpty {
+            if let top = try? other.pop() {
+                guard let topElement = top  else { break }
+                nStack.push(element: topElement)
+            }
+        }
+        return nStack
     }
 }
 
